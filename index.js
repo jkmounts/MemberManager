367a5c9c7a17f956ea3af2960f1546004d521d76
Jestin Mounts <jkmounts@gmail.com>
2021-06-06 17:44:58 -0400

    Form submissions are added to database

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(3000, () => console.log('listening at 3000...'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response) => {
-    console.log('I got a request');
-    console.log(request.body);
+    console.log('Post request received');
+    const data = request.body;
+    database.insert(data);
 
     response.json({
         status: "success"
