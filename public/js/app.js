290c6cbd1c83ca6799207b3c1d187ac7c260c8b8
Jestin Mounts <jkmounts@gmail.com>
2021-06-06 17:32:22 -0400

    Posts created member object to Server

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,23 @@ console.log("app.js connected");
 const addMemberForm = document.querySelector('#addMember');
 addMemberForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    addMembertoDB(e);
+});
+
+async function addMembertoDB(e) {
+    // Create member object with values from form on page
     let name = e.target.querySelector("input[name='name']").value;
     let email = e.target.querySelector("input[name='email']").value;
     let member = new Member(name, email);
-    console.log(member);
-});
+    
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(member),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const response = await fetch('/api', options);
+    const json = await response.json();
+    console.log(json);
+}
