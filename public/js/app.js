240c748b3192507ee65a7c12347982774dd72c93
Jestin Mounts <jkmounts@gmail.com>
2021-06-08 16:29:57 -0400

    Created request to member db

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,19 @@ console.log("app.js connected");
 const addMemberForm = document.querySelector('#addMember');
 addMemberForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    addMembertoDB(e);
+    createMember(e);
 });
 
-function addMembertoDB(e) {
+function createMember(e) {
     // Create member object with values from form on page
     let name = e.target.querySelector("input[name='name']").value;
     let email = e.target.querySelector("input[name='email']").value;
     let member = new Member(name, email);
     member.addToDB();
+}
+
+async function getMembers() {
+    const response = await fetch('/api');
+    const data = await response.json();
+    console.log(data);
 }
