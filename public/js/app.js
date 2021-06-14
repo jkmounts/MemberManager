0e31c0ff514af3ff4d7b8ba89cd68e12d85cf939
Jestin Mounts <jkmounts@gmail.com>
2021-06-14 15:21:34 -0400

    Reorganized app.js functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,31 @@
-console.log("app.js connected");
-
-getMembers();
-
-const addMemberForm = document.querySelector('#addMember');
-addMemberForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    createMember(e);
-    refreshMembers();
-    e.target.reset();
-});
-
-function createMember(e) {
-    // Create member object with values from form on page
-    let name = e.target.querySelector("input[name='name']").value;
-    let email = e.target.querySelector("input[name='email']").value;
-    let member = new Member(name, email);
-    member.addToDB();
-}
-
 const memberList = document.querySelector('#memberList');
 
+// Function retrieves members from db and adds to page
 async function getMembers() {
     const response = await fetch('/api');
     const data = await response.json();
     data.forEach(member => {
         createMemberDiv(member, memberList);
     });
+}
+
+getMembers();
+
+const addMemberForm = document.querySelector('#addMember');
+// Event Listener to create member when form is submitted and refersh page display
+addMemberForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createMember(e.target);
+    refreshMembers();
+    e.target.reset();
+});
+
+function createMember(form) {
+    // Create member object with values from form on page
+    let name = form.querySelector("input[name='name']").value;
+    let email = form.querySelector("input[name='email']").value;
+    let member = new Member(name, email);
+    member.addToDB();
 }
 
 function refreshMembers() {
